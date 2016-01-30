e4b1db62dc1c9084c896cc01479b25e8feaeb3b5
Vyacheslav Belous <v.belous@corp.mail.ru>
2016-01-31 01:04:23 +0300

    bootloader: implement `elf' loading

---
+++ src/boot/elf.h
@@ -0,0 +1,94 @@
+#ifndef __ELF_H__
+#define __ELF_H__
+
+#include <stdint.h>
+
+#define ELF_MAGIC 0x464C457FU /* "\x7FELF" in little endian */
+
+#ifdef __i386__
+
+typedef uint32_t Elf32_Addr;
+typedef uint16_t Elf32_Half;
+typedef uint32_t Elf32_Off;
+typedef int32_t  Elf32_Sword;
+typedef uint32_t Elf32_Word;
+
+struct elf32_header {
+	Elf32_Word e_magic;	// elf magic, must be equal to `ELF_MAGIC'
+	uint8_t e_elf[12];	// elf identification
+	Elf32_Half e_type;	// object file type
+	Elf32_Half e_machine;	// machine type
+	Elf32_Word e_version;	// object file version
+	Elf32_Addr e_entry;	// entry point address
+	Elf32_Off e_phoff;	// program header offset
+	Elf32_Off e_shoff;	// section header offset
+	Elf32_Word e_flags;	// processor-specific flags
+	Elf32_Half e_ehsize;	// elf header size
+	Elf32_Half e_phentsize;	// size of program header entry
+	Elf32_Half e_phnum;	// number of program header entries
+	Elf32_Half e_shentsize;	// size of section header entry
+	Elf32_Half e_shnum;	// number of section header entries
+	Elf32_Half e_shstrndx;	// section name string table index
+};
+
+struct elf32_program_header {
+	Elf32_Word p_type;	// type of segment
+	Elf32_Off p_offset;	// offset in file
+	Elf32_Addr p_va;	// virtual address in memory
+	Elf32_Addr p_pa;	// reserved
+	Elf32_Word p_filesz;	// size of segment in file
+	Elf32_Word p_memsz;	// size of segment in memory
+	Elf32_Word p_flags;	// segment attributes
+	Elf32_Word p_align;	// aligment of segment
+};
+
+#else
+
+typedef uint64_t Elf64_Addr;
+typedef uint64_t Elf64_Off;
+typedef uint16_t Elf64_Half;
+typedef uint32_t Elf64_Word;
+typedef int32_t Elf64_Sword;
+typedef uint64_t Elf64_Xword;
+typedef int64_t Elf64_Sxword;
+
+struct elf64_header {
+	Elf64_Word e_magic;	// elf magic, must be equal to `ELF_MAGIC'
+	uint8_t e_elf[12];	// elf identification
+	Elf64_Half e_type;	// object file type
+	Elf64_Half e_machine;	// machine type
+	Elf64_Word e_version;	// object file version
+	Elf64_Addr e_entry;	// entry point address
+	Elf64_Off e_phoff;	// program header offset
+	Elf64_Off e_shoff;	// section header offset
+	Elf64_Word e_flags;	// processor-specific flags
+	Elf64_Half e_ehsize;	// elf header size
+	Elf64_Half e_phentsize;	// size of program header entry
+	Elf64_Half e_phnum;	// number of program header entries
+	Elf64_Half e_shentsize;	// size of section header entry
+	Elf64_Half e_shnum;	// number of section header entries
+	Elf64_Half e_shstrndx;	// section name string table index
+};
+
+struct elf64_program_header {
+	Elf64_Word p_type;	// type of segment
+	Elf64_Word p_flags;	// segment attributes
+	Elf64_Off p_offset;	// offset in file
+	Elf64_Addr p_va;	// virtual address in memory
+	Elf64_Addr p_pa;	// reserved
+	Elf64_XWord p_filesz;	// size of segment in file
+	Elf64_XWord p_memsz;	// size of segment in memory
+	Elf64_XWord p_align;	// aligment of segment
+};
+
+#endif // __i386__
+
+// p_type
+#define ELF_PHEADER_TYPE_LOAD	1
+
+// p_flags
+#define ELF_PHEADER_FLAG_EXEC	0x1
+#define ELF_PHEADER_FLAG_WRITE	0x2
+#define ELF_PHEADER_FLAG_READ	0x4
+
+#endif // __ELF_H__
--- src/boot/main.c
+++ src/boot/main.c
@@ -1,14 +1,33 @@
 #include <ata.h>
+#include <elf.h>
 
-#define KERNEL_LOADER_ADDRESS	0x100000 // 1MB
+#define KERNEL_LOADER_ADDRESS	0x10000
 
 __attribute__((noreturn))
 void bootmain(void)
 {
-	uint8_t *binary = (uint8_t *)KERNEL_LOADER_ADDRESS;
+	struct elf32_header *elf_header = (struct elf32_header *)KERNEL_LOADER_ADDRESS;
+	struct elf32_program_header *ph, *eph;
 
-	if (disk_io_read_segment((uint32_t)binary, ATA_SECTOR_SIZE, 0) != 0)
+	// Start read from sector `1', because sector `0' contains bootloader
+	if (disk_io_read_segment((uint32_t)elf_header, ATA_SECTOR_SIZE, 1) != 0)
 		goto error;
+
+	if (elf_header->e_magic != ELF_MAGIC)
+		goto error;
+
+	ph = (struct elf32_program_header *)((uint8_t *)elf_header + elf_header->e_phoff);
+	eph = ph + elf_header->e_phnum;
+	for (; ph < eph; ph++) {
+		// `+1' because kernel starts in sector `1' (not `0')
+		uint32_t lba = (ph->p_offset / ATA_SECTOR_SIZE) + 1;
+
+		if (disk_io_read_segment(ph->p_va, ph->p_memsz, lba) != 0)
+			goto error;
+	}
+
+	// call entry
+	((void (*)(void))(elf_header->e_entry))();
 
 error:
 	while (1)
