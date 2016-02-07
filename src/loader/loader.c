6ed0620dd15b0357b38371c7562f447d28fad110
Vyacheslav Belous <v.belous@corp.mail.ru>
2016-02-07 19:51:58 +0300

    Detect memory map using bios (INT 15h, EAX=E820h)

--- src/loader/loader.c
+++ src/loader/loader.c
@@ -1,5 +1,22 @@
+#include <stdint.h>
+
+struct mm_entry {
+	uint64_t base_addr;
+	uint64_t addr_len;
+	uint32_t type;
+	uint32_t acpi_attrs;
+};
+
+#define BOOT_MM_ADDR	(void *)0x7e00
+
 void loader_main(void)
 {
+	struct mm_entry *mm = BOOT_MM_ADDR;
+	uint32_t cnt = *((uint32_t *)BOOT_MM_ADDR - 1);
+
+	(void)mm;
+	(void)cnt;
+
 	while (1)
 		/*do nothing*/;
 }
